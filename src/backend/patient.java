56903ec8b372b954358655cc94e8223b679dc127
sejal desai <sej@sejals-MacBook-Pro.local>
2022-10-25 17:21:35 -0400

    login complete

---
+++ src/backend/patient.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ *
+ * @author sej
+ */
+public class patient {
+
+    public String person;
+    public int contactNo;
+    public String EmailId;
+    public String gender;
+    public String Age;
+    public String communityName;
+    public String NewCity;
+    public float Height;
+    public float Weight;
+    public String State;
+    
+    
+}
---
+++ src/backend/person.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ *
+ * @author sej
+ */
+public class person {
+    public String name;
+    public int Age;
+    public long contact;
+    public String emailid;
+    public String gender;
+    public String personId;
+    public long house;
+    public House = residence house();
+    public String communityname;
+    public String NewCity;
+    public String State;
+    public String residence;
+    public String PersonId; 
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public long getHouse() {
+        return house;
+    }
+
+    public void setHouse(long house) {
+        this.house = house;
+    }
+
+    public String getCommunityname() {
+        return communityname;
+    }
+
+    public void setCommunityname(String communityname) {
+        this.communityname = communityname;
+    }
+
+    public String getNewCity() {
+        return NewCity;
+    }
+
+    public void setNewCity(String NewCity) {
+        this.NewCity = NewCity;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Object getPersonId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   
+    
+    
+    
+    
+    
+}
